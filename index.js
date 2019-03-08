a2abc13d7edaaf912e22debfa3a4c2fd936982ef
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2019-03-08 02:37:45 -0300

    Document the implementation

--- index.js
+++ index.js
@@ -33,23 +33,58 @@ var EventEmitter = (() => {
 	};
 
 
+	/**
+	 * Event Emitter base class. All methods are defined in constructor.
+	 * @class
+	 */
 	var EventEmitter = function() {
 
 		var eventRegistry = {};
 
-
+		/**
+		 * Binds a callback to an event. The callback will be called with the
+		 * eventObject dispatched by the dispatchEvent method.
+		 *
+		 * @method addEventListener
+		 * @memberof EventEmitter
+		 * @param  {string}   eventName The event to which the callback should be
+		 * bound.
+		 * @param  {Function} callback  The callback to be called when the event
+		 * is triggered.
+		 */
 		this.addEventListener = function(eventName, callback) {
 
 			addEventListener(eventRegistry, eventName, callback);
 
 		};
 
+
+		/**
+		 * Removes a callback from the callback list of an event.
+		 *
+		 * @method removeEventListener
+		 * @memberof EventEmitter
+		 * @param  {string}   eventName The event from which the callback should
+		 * be removed.
+		 * @param  {Function} callback  The callback to be called when the event
+		 * is triggered.
+		 */
 		this.removeEventListener = function(eventName, callback) {
 
 			removeEventListener(eventRegistry, eventName, callback);
 
 		};
 
+
+		/**
+		 * Fires the callbacks of the event specified by the type property of the
+		 * eventObject.
+		 *
+		 * @method dispatchEvent
+		 * @memberof EventEmitter
+		 * @param  {Object} eventObject The event object to be passed to the
+		 * callbacks. Its type property defines which event will be fired.
+		 */
 		this.dispatchEvent = function(eventObject) {
 
 			dispatchEvent(eventRegistry, eventObject);
